f784ab9643aea73db68708201d7eeb60fbf90b66
Gun <cobaltgun2324@gmail.com>
2023-12-01 15:00:13 +0700

    feat: add complete exercise

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,13 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+  if (numbers[i] < minNumber) {
+    minNumber = numbers[i];
+  } else if (minNumber === undefined) {
+    minNumber = numbers[i];
+  }
+}
 
 console.log(minNumber);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,7 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for (let text of companyName) {
+  reversedCompanyName = text + reversedCompanyName;
+}
 console.log(reversedCompanyName);
